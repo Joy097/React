6da608539aa40c0afd6b92e8dd077096fd3e1951
Joy097 <shihabrumeechowdhury@gmail.com>
2023-10-06 18:29:48 +0600

    update 30

--- Code snippets/easy_counter_app/React/script.js
+++
@@ -1,8 +0,0 @@
-let numb = 0;
-const button = document.getElementById('button');
-const display = document.getElementById('display');
-
-button.addEventListener('click',() =>{
-    numb++;
-    display.textContent = numb
-});
