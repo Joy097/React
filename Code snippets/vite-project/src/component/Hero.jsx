a3355dc00d5adee1b4d19049015bffffb2ceaf60
Joy097 <shihabrumeechowdhury@gmail.com>
2023-10-29 16:55:17 +0600

    update 278

--- Code snippets/vite-project/src/component/Hero.jsx
+++ Code snippets/vite-project/src/component/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Hero = () => {
     return (
         <div>
-            
+            <img src='https://media.licdn.com/dms/image/C4D03AQF51JuSnxV_2w/profile-displayphoto-shrink_200_200/0/1656353622941?e=1704326400&v=beta&t=_gQKzZU5h6DO9mYDXvx9mm8DI5S7PX9aFVnBRSHeulI'/>
         </div>
     );
 };
