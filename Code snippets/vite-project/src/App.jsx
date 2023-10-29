333d2052614033d0ff35c6590f7922b61768370a
Joy097 <shihabrumeechowdhury@gmail.com>
2023-10-29 16:47:52 +0600

    update 271

--- Code snippets/vite-project/src/App.jsx
+++ Code snippets/vite-project/src/App.jsx
@@ -1,7 +1,9 @@
+import Demo from "./Demo";
 
 const App = () => {
   return (
     <div>
+      <Demo/>
       <h1>I am learning react</h1>
       <input placeholder="My Name"/>
       <button>Submit</button>
