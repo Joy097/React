b0a458343ad1d4deb7b679529b8e45a074903207
Joy097 <shihabrumeechowdhury@gmail.com>
2023-10-29 16:57:25 +0600

    update 280

--- Code snippets/vite-project/src/App.jsx
+++ Code snippets/vite-project/src/App.jsx
@@ -1,12 +1,15 @@
+import ContactForm from "./component/ContactForm";
+import Footer from "./component/Footer";
 import Header from "./component/Header";
-
+import
 
 const App = () => {
   return (
     <div>
       <Header/>
       <Hero />
-      
+      <ContactForm/>
+      <Footer/>
     </div>
   );
 };
