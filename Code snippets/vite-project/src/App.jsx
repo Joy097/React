c9bcc2234ecfe56e17c08b3647a48d211b080736
Joy097 <shihabrumeechowdhury@gmail.com>
2023-10-30 22:59:38 +0600

    update 45

--- Code snippets/vite-project/src/App.jsx
+++ Code snippets/vite-project/src/App.jsx
@@ -18,6 +18,12 @@ const App = () => {
         })()}
 
         <ul>
+          {
+            arr.map((item,i)=>{
+              return<li key={i.toString()}>{item}</li>
+              //key is to catch the iteration number
+            })
+          }
         </ul>
     </div>
   );
