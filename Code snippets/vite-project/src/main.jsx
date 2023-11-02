25268d89198aaeb68171fffbeb851ee956abc9af
Joy097 <shihabrumeechowdhury@gmail.com>
2023-11-02 23:38:23 +0600

    update 79

--- Code snippets/vite-project/src/main.jsx
+++ Code snippets/vite-project/src/main.jsx
@@ -5,7 +5,7 @@ import CDRen_switch from './CDRen_switch'
 import CDRen_ternary from './CDRen_ternary'
 import CDRen_logi_and from './CDRen_logi_and'
 import CDRen_Im_invoke from './CDRen_Im_invoke'
-import parent from './parent';
+import PARENT from './PARENT';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -14,6 +14,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <CDRen_ternary/>
     <CDRen_logi_and/>
     <CDRen_Im_invoke/>
-    <parent/>
+    <PARENT/>
   </React.StrictMode>,
 )
--- Code snippets/vite-project/src/parent.jsx
+++ Code snippets/vite-project/src/parent.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
 
-const parent = () => {
+const PARENT = () => {
     return (
         <div>
             
@@ -8,4 +7,4 @@ const parent = () => {
     );
 };
 
-export default parent;
+export default PARENT;
