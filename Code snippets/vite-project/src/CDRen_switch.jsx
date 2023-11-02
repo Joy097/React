f767f624f8c46896c52fe72d691b1311632557f9
Joy097 <shihabrumeechowdhury@gmail.com>
2023-11-02 22:43:49 +0600

    update 28

--- Code snippets/vite-project/src/CDRen_switch.jsx
+++ Code snippets/vite-project/src/CDRen_switch.jsx
@@ -1,15 +1,11 @@
-const header = ()=>{
-    return <h1>Login status(switch)</h1>
-};
 
 const CDRen_switch = () => {
     const status = true;
-    header();
     switch(status){
         case true:
             return <button>Logout</button>
         case false:
-            return <button>Login</button>
+            return (<button>Login</button>) 
         default:
             return null
     }
