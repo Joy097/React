1e88aa6ee414d5bdc995468fd5717a6d6bfc7c69
Joy097 <shihabrumeechowdhury@gmail.com>
2023-11-02 23:27:44 +0600

    update 69

--- Code snippets/vite-project/src/CDRen_Im_invoke.jsx
+++ Code snippets/vite-project/src/CDRen_Im_invoke.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
 const CDRen_Im_invoke = () => {
+    const status = true;
     return (
         <div>
             <h1>Login status (Immediately invoke) </h1>
             {(()=>{
                 if(status==true){
-                    return 
+                    return <button>Logout Button</button>
+                }
+                else{
+                    return <
                 }
             })()}
         </div>
