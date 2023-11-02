0b7ba5274853855a55883915e1367e3b0b2c8533
Joy097 <shihabrumeechowdhury@gmail.com>
2023-11-03 00:03:56 +0600

    update 103

--- Code snippets/vite-project/src/parent.jsx
+++ Code snippets/vite-project/src/parent.jsx
@@ -1,9 +1,15 @@
 import Child1 from "./component/Child1";
 
 const PARENT = () => {
+    const item = {
+        name: "Joy",
+        
+    }
     return (
         <div>
-            <Child1 title="This is passing props"
+            <Child1 
+
+            title="This is passing props"
             description="This passing props description"/>
         </div>
     );
