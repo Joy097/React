031ca90dc1ab26af3936b888d93667e8025eaba0
Joy097 <shihabrumeechowdhury@gmail.com>
2023-11-02 22:28:47 +0600

    update 14

--- Code snippets/vite-project/src/CDRen_ifelse.jsx
+++ Code snippets/vite-project/src/CDRen_ifelse.jsx
@@ -1,29 +1,15 @@
 const LoginStatus=(status)=>{
     if (status){
-        return 
+        return <button></button>
     }
 }
 
 
 
 const CDRen_ifelse = () => {
-    const status = false;
-    if (status==true) {
     return (
-        <div>
-            <h1 className="">Logged in</h1>
-            <button>Logout</button>
-        </div>
-    );}
-
-    else{
-        return (
-            <div>
-                <h1 className="">Logged out</h1>
-                <button>Login</button>
-            </div>
-        );
-    }
+        <
+    );
 };
 
 export default CDRen_ifelse;
