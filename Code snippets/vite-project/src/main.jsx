b06393c4059fb73c37d8394c3428cc0d6e07f134
Joy097 <shihabrumeechowdhury@gmail.com>
2023-11-02 22:18:17 +0600

    update 6

--- Code snippets/vite-project/src/main.jsx
+++ Code snippets/vite-project/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import CD_format from './CD_format'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    
+    <CD_format/>
   </React.StrictMode>,
 )
