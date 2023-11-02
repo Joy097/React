0562789508b8918f811553b25be90d191b80ead8
Joy097 <shihabrumeechowdhury@gmail.com>
2023-11-02 23:37:19 +0600

    update 78

--- Code snippets/vite-project/src/main.jsx
+++ Code snippets/vite-project/src/main.jsx
@@ -5,6 +5,7 @@ import CDRen_switch from './CDRen_switch'
 import CDRen_ternary from './CDRen_ternary'
 import CDRen_logi_and from './CDRen_logi_and'
 import CDRen_Im_invoke from './CDRen_Im_invoke'
+import parent from './parent';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -13,5 +14,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <CDRen_ternary/>
     <CDRen_logi_and/>
     <CDRen_Im_invoke/>
+    <parent/>
   </React.StrictMode>,
 )
