c4b59e0fd25e2f77446725a2062a1cee56749fff
Joy097 <shihabrumeechowdhury@gmail.com>
2023-11-02 23:15:55 +0600

    update 58

--- Code snippets/vite-project/src/CDRen_logi_and.jsx
+++ Code snippets/vite-project/src/CDRen_logi_and.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const CDRen_logi_and = () => {
     return (
         <div>
-            
+            <h1>Login status(Logical &&)</h1>
+            {status && }
         </div>
     );
 };
