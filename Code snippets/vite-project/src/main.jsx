ca7f1b72bb924fdb5dfc277aa774cfd9e8cbb342
Joy097 <shihabrumeechowdhury@gmail.com>
2023-11-02 23:18:02 +0600

    update 60

--- Code snippets/vite-project/src/main.jsx
+++ Code snippets/vite-project/src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import CDRen_ifelse from './CDRen_ifelse'
 import CDRen_switch from './CDRen_switch'
 import CDRen_ternary from './CDRen_ternary'
+import CDRen_logi_and from './CDRen_logi_and'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CDRen_ifelse/>
     <CDRen_switch/>
     <CDRen_ternary/>
+    <CDRen_logi_and/>
   </React.StrictMode>,
 )
