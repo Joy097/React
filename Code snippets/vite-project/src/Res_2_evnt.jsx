3e4e3f77d5a7b299eceb368b5e252266b6449bbc
Joy097 <shihabrumeechowdhury@gmail.com>
2023-11-04 21:32:47 +0600

    update 24

--- Code snippets/vite-project/src/Res_2_evnt.jsx
+++ Code snippets/vite-project/src/Res_2_evnt.jsx
@@ -3,6 +3,7 @@ const Res_2_evnt = () => {
     return (
         <div>
             <button onClick={alert("Click")}>Onclick handler direct</button>
+            <button onClick={()=>< onClick={alert("Click")}>Onclick handler direct</button>
         </div>
     );
 };
