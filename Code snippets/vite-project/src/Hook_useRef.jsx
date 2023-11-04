494aebdb4bd44e7c867407b8a5f31a81ad5dcd51
Joy097 <shihabrumeechowdhury@gmail.com>
2023-11-04 22:26:23 +0600

    update 73

--- Code snippets/vite-project/src/Hook_useRef.jsx
+++ Code snippets/vite-project/src/Hook_useRef.jsx
@@ -4,7 +4,8 @@ const Hook_useRef = () => {
     const Header = useRef()
     return (
         <div>
-            <h1 ref=></h1>
+            <h1 ref={Header}></h1>
+            <button onClick={Hook_useRef}></button>
         </div>
     );
 };
