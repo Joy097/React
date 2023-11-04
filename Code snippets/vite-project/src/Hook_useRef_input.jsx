887dc89b02fa6c5c216fb3e3299a0bc46b889065
Joy097 <shihabrumeechowdhury@gmail.com>
2023-11-04 22:55:14 +0600

    update 100

--- Code snippets/vite-project/src/Hook_useRef_input.jsx
+++ Code snippets/vite-project/src/Hook_useRef_input.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const Hook_useRef_input = () => {
     return (
         <div>
-            
+            <input placeholder='FirstName'/>
+            <input placeholder='FirstName'/>
         </div>
     );
 };
