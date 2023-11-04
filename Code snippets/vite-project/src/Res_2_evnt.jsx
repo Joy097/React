51fab395af7b90f12b47b3b8a13c2d157fc8de5c
Joy097 <shihabrumeechowdhury@gmail.com>
2023-11-04 21:37:03 +0600

    update 28

--- Code snippets/vite-project/src/Res_2_evnt.jsx
+++ Code snippets/vite-project/src/Res_2_evnt.jsx
@@ -1,11 +1,12 @@
 // Onclick handler
 const Res_2_evnt = () => {
     function Demo(){
-        alert('')
+        alert('Normal Function')
     }
     return (
         <div>
             <button onClick={alert("Click")}>Onclick handler direct</button>
+            <button onClick={()=>{alert("Click")}}>Onclick handler after click</button>
             <button onClick={()=>{alert("Click")}}>Onclick handler after click</button>
         </div>
     );
