0d392ade4e128eda82fcc8ef03b56a928fb0e73e
Joy097 <shihabrumeechowdhury@gmail.com>
2023-11-04 21:48:58 +0600

    update 38

---
+++ Code snippets/vite-project/src/Res_2_evnt_Form.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Res_2_evnt_Form = () => {
+    return (
+        <div>
+            <form>
+                <input></input>
+            </form>
+        </div>
+    );
+};
+
+export default Res_2_evnt_Form;
