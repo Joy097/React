8eb26ba1e00b1e531ee13f15dc7041654130d4ff
Joy097 <shihabrumeechowdhury@gmail.com>
2023-11-04 22:29:34 +0600

    update 76

--- Code snippets/vite-project/src/Hook_useRef.jsx
+++ Code snippets/vite-project/src/Hook_useRef.jsx
@@ -1,10 +1,12 @@
 import {useRef} from 'react';
 
 const Hook_useRef = () => {
+
     const Header = useRef()
-    function action(){
+    const action = ()=>{
         Header.current.innerHTML = <ul><li>AMM</li><li>JAM</li></ul>
     }
+    
     return (
         <div>
             <h1 ref={Header}></h1>
