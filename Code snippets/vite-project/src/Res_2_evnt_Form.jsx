feca370d5227585b3ba9ffa302a2543ed770c56b
Joy097 <shihabrumeechowdhury@gmail.com>
2023-11-04 21:55:27 +0600

    update 44

--- Code snippets/vite-project/src/Res_2_evnt_Form.jsx
+++ Code snippets/vite-project/src/Res_2_evnt_Form.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 const Res_2_evnt_Form = () => {
 
     const PostFormData=()=>{
-        
+
     };
 
     return (
         <div>
-            <form onSubmit={}>
+            <form onSubmit={PostFormData}>
                 <input/>
                 <button type="submit">Submit</button>
             </form>
