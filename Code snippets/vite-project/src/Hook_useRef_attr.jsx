7093f40d091a0093379db39d79c8f542a25c7ca3
Joy097 <shihabrumeechowdhury@gmail.com>
2023-11-04 22:40:13 +0600

    update 86

--- Code snippets/vite-project/src/Hook_useRef_attr.jsx
+++ Code snippets/vite-project/src/Hook_useRef_attr.jsx
@@ -1,10 +1,11 @@
 import {useRef} from 'react';
 
 const Hook_useRef_attr = () => {
-    
+
+    const hook1 = useRef()
     return (
         <div>
-            
+            <img src='https://i.postimg.cc/6QyTynzr/bulb-on.png'></img>
         </div>
     );
 };
