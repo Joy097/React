9b3fc7d23fafba6e728c61be64b1bae71dbfa408
Joy097 <shihabrumeechowdhury@gmail.com>
2023-11-04 22:32:46 +0600

    update 79

--- Code snippets/vite-project/src/main.jsx
+++ Code snippets/vite-project/src/main.jsx
@@ -8,6 +8,7 @@ import CDRen_Im_invoke from './CDRen_Im_invoke'
 import PARENT from './PARENT';
 import Res_2_evnt_Btn from './Res_2_evnt_Btn'
 import Res_2_evnt_Form from './Res_2_evnt_Form'
+import Hook_useRef from './Hook_useRef'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -19,6 +20,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <PARENT/>
     <Res_2_evnt_Btn/>
     <Res_2_evnt_Form/>
-    <
+    <Hook_useRef/>
   </React.StrictMode>,
 )
