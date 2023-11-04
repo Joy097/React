acb35a7cbccb2e32f8bf311cf474a5ac4656afaf
Joy097 <shihabrumeechowdhury@gmail.com>
2023-11-04 22:44:34 +0600

    update 90

--- Code snippets/vite-project/src/Hook_useRef_attr.jsx
+++ Code snippets/vite-project/src/Hook_useRef_attr.jsx
@@ -5,11 +5,12 @@ const Hook_useRef_attr = () => {
     const hook1 = useRef()
     const action = ()=>{
         hook1.current.src = 'https://i.postimg.cc/KjK1wL3c/bulb-off.png'
+        hook1.setAttribute
     }
     return (
         <div>
             <img ref={hook1} src='https://i.postimg.cc/6QyTynzr/bulb-on.png'></img>
-            <button>Lights</button>
+            <button onClick={action}>Lights</button>
         </div>
     );
 };
