698bd66e72f4a331ba6cbf31dca8fe4301b9e676
Joy097 <shihabrumeechowdhury@gmail.com>
2023-11-05 23:55:36 +0600

    update 30

--- Code snippets/vite-project/src/Hook_useRef_css.jsx
+++ Code snippets/vite-project/src/Hook_useRef_css.jsx
@@ -2,11 +2,12 @@ import {useRef} from 'react';
 
 const Hook_useRef_css = () => {
     const change = () =>{
-        
+        let cssHeader={useRef}
+        cssHeader.
     }
     return (
         <div>
-            <h2 className='text-success'>This is a css change for useRef</h2>
+            <h2 ref={cssHeader} className='text-success'>This is a css change for useRef</h2>
             <button onClick={change}> Change CSS</button>
         </div>
     );
