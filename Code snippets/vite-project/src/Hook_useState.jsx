c22f4200c7d9047dede25b1b8bc966d0d4fceb80
Joy097 <shihabrumeechowdhury@gmail.com>
2023-11-07 22:55:24 +0600

    update 42

--- Code snippets/vite-project/src/Hook_useState.jsx
+++ Code snippets/vite-project/src/Hook_useState.jsx
@@ -1,10 +1,15 @@
 import React,{useState} from 'react';
 
 const Hook_useState = () => {
-    const [num, setNum] = 
+    const [num, setNum] = useState(0);
+    const change = ()=>{
+
+    }
+
     return (
         <div>
             <h2>useState Increase: </h2>
+            <button onClick={change}></button>
         </div>
     );
 };
