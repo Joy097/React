a545b485424298f667d772cae3299ac5ba49234a
Joy097 <shihabrumeechowdhury@gmail.com>
2023-11-07 22:49:01 +0600

    update 36

--- Code snippets/vite-project/src/Hook_useState.jsx
+++ Code snippets/vite-project/src/Hook_useState.jsx
@@ -1,8 +1,11 @@
-import { useState } from "react";
-import React,useState from 'react';
+import React, {useState}  from 'react';
 
 const Hook_useState = () => {
     const [number,setNum] = useState();
+    const setNum = () => {
+        
+    }
+
     return (
         <div>
             <h2>Number: </h2>
