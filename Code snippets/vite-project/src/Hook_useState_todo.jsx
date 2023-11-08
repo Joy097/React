f986c85736c014e989fdcb43bfab3cb681789dac
Joy097 <shihabrumeechowdhury@gmail.com>
2023-11-08 07:47:28 +0600

    update 1

--- Code snippets/vite-project/src/Hook_useState_todo.jsx
+++ Code snippets/vite-project/src/Hook_useState_todo.jsx
@@ -1,6 +1,11 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 const Hook_useState_todo = () => {
+    const Hook = useState({
+        key1: 'key1',
+        key1: 'key1',
+        key1: 'key1',
+    })
     return (
         <div>
             
