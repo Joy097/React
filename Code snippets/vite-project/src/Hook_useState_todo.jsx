52a749313a43164e31758bd7ac51d26a688708b2
Joy097 <shihabrumeechowdhury@gmail.com>
2023-11-08 07:46:24 +0600

    update 0

--- Code snippets/vite-project/src/Hook_useState_todo.jsx
+++ Code snippets/vite-project/src/Hook_useState_todo.jsx
@@ -1,14 +1,9 @@
-import React,{useState} from 'react';
+import React from 'react';
 
 const Hook_useState_todo = () => {
-    const [list, setList] = useState([]);
-    const [item, setItem] = useState("");
     return (
         <div>
-            <h1>ToDo :</h1>
-            <p>{item}</p>
-            <input onChange={(e)=>{setItem(e.target.value)}}/>
-            <button>Add</button>
+            
         </div>
     );
 };
