f98ea9c32420c83ababec40d42a34b88f9e42e4c
Joy097 <shihabrumeechowdhury@gmail.com>
2023-11-08 08:08:38 +0600

    update 21

--- Code snippets/vite-project/src/Hook_useState_todo.jsx
+++ Code snippets/vite-project/src/Hook_useState_todo.jsx
@@ -4,9 +4,12 @@ const Hook_useState_todo = () => {
     const [list, setList] = useState([])
     const [item, setItem] = useState('')
 
+    const add2List = ()=>{
+
+    }
     return (
         <div>
-            <button></button>
+            <button onClick={add2List}>Add</button>
         </div>
     );
 };
