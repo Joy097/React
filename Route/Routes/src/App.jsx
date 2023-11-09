8d4db0dff1de57a63c8f61edc12921ae2edae89a
Joy097 <shihabrumeechowdhury@gmail.com>
2023-11-10 00:18:31 +0600

    update 33

--- Route/Routes/src/App.jsx
+++ Route/Routes/src/App.jsx
@@ -1,6 +1,8 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import notFound from './pages/notFound';
 import home from './pages/home';
+import home from './pages/product';
+import profile from './pages/profile';
 
 const App = () => {
   return (
@@ -9,7 +11,8 @@ const App = () => {
       <Routes>
         <Route path='' element={<home/>}/>
         <Route path='' element={<notFound/>}/>
-        <Route path='' element={<CSSMathProduct}/>
+        <Route path='' element={<product/>}/>
+        <Route path='' element={<profile/>}/>
       </Routes>
       </BrowserRouter>
     </div>
