75cbcd1d1f9f532732069850d5789afe35a50819
Joy097 <shihabrumeechowdhury@gmail.com>
2023-11-10 00:36:11 +0600

    update 49

--- Route/Routes/src/App.jsx
+++ Route/Routes/src/App.jsx
@@ -1,20 +1,8 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import notFound from './pages/notFound.jsx';
-import home from './pages/home.jsx';
-import product from './pages/product.jsx';
-import profile from './pages/profile.jsx';
 
 const App = () => {
   return (
     <div>
-      <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<home/>}/>
-        <Route path='*' element={<notFound/>}/>
-        <Route path='/product' element={<product/>}/>
-        <Route path='/profile' element={<profile/>}/>
-      </Routes>
-      </BrowserRouter>
+      <h1>jasghdajh</h1>
     </div>
   );
 };
