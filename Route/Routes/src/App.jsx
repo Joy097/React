bdc6457b11dbbf70665bed9323ec386ee933e0e8
Joy097 <shihabrumeechowdhury@gmail.com>
2023-11-10 00:16:23 +0600

    update 31

--- Route/Routes/src/App.jsx
+++ Route/Routes/src/App.jsx
@@ -1,11 +1,13 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import notFound from './pages/notFound';
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
       <Routes>
-        <Route path='' element=''/>
+        <Route path='' element={<home/>}/>
+        <Route path='' element={<notFound}/>
       </Routes>
       </BrowserRouter>
     </div>
