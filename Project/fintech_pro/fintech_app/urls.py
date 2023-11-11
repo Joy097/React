7b7ee062d3eadb32dcf83e9bba73c75970989ac2
Joy097 <shihabrumeechowdhury@gmail.com>
2023-11-12 00:38:21 +0600

    update 147

--- Project/fintech_pro/fintech_app/urls.py
+++ Project/fintech_pro/fintech_app/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path('register/',views.Register.as_view()),
     path('login/',views.Login.as_view()),
     path('user/',views.UserView.as_view()),
+    path('logout/',views.Logout.as_view()),
 ]
